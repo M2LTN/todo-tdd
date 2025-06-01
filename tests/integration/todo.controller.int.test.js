45e86025e3694b3e21b8c24917f8f09d459905d4
M2LTN <aaron.malton@voco.ee>
2025-06-01 18:11:16 +0300

    Created TodoController.getTodos unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -21,6 +21,15 @@ describe(endpointUrl, () => {
     exppect(response.body).toStrictEqual({
       message: "Todo validation failed: done: Path `done` is required.",
     });
+
+});
+test("GET " + endpointUrl, async () => {
+  const response = await request(app).get(endpointUrl)
+  expect(response.statusCode).toBe(200)
+  expect(Array.isArray(response.body)).toBeTruthy()
+  expect(response.body[0].title).toBeDefined()
+  expect(response.body[0].done).toBeDefined()
+}); 
+
   });
   
-});
