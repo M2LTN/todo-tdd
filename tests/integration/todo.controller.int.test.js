9a808b603f60939d2cb4f9081736a3e9109d45d9
M2LTN <aaron.malton@voco.ee>
2025-06-02 15:15:15 +0300

    Created TodoController.updateTodo unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,8 +4,12 @@ const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
-let firstTodo
-
+let firstTodo, newTodoId;
+const testData = {
+  title: "Make integration test for PUT",
+  done: true
+};
+const notExistingTodoId = "683c4feb83b48baa4c6b2222";
 
 describe(endpointUrl, () => {
   it("POST " + endpointUrl, async () => {
@@ -15,6 +19,7 @@ describe(endpointUrl, () => {
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+    newTodoId = response.body._id;
   });
   it("should return error 500 on malformed data with POST" + endpointUrl, async () => {
     const response = await request(app)
@@ -47,4 +52,24 @@ describe(endpointUrl, () => {
     .get(endpointUrl + "683c4feb83b48baa4c6b2222");
     expect(response.statusCode).toBe(404);
   });
+
+  it("PUT " + endpointUrl, async () => {
+    const testData = {
+      title: "Make integration test for PUT",
+      done: true
+    };
+    const res = await request(app)
+      .put(endpointUrl + newTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(testData.title);
+    expect(res.body.done).toBe(testData.done);
+  });
+  it("should return 404 on PUT " + endpointUrl, async () => {
+    const res = await request(app)
+      .put(endpointUrl + notExistingTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(404);
+  });  
+  
 });
